76b54f5129eca688fd173ff718977bfe291c4083
Gihan Chamila <algihanchamila@gmail.com>
2024-04-23 14:52:49 +0530

    For features, an array created for cards
    Added descriptions in features array for every card item and, added svgs for features card items.

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,3 +29,33 @@ export const navigation = [
         url: "#contactUs",
     }
 ]
+
+export const features = [
+    {
+        id: "1",
+        icon: "",
+        title: "Personalized Learning",
+        description: "Tailored quizzes based on your current IQ level ensure a customized learning experience"
+    },
+
+    {
+        id: "2",
+        icon: "",
+        title: "Comprehensive Assessment",
+        description: "Test your IQ level across various categories including logic, mathematics, language, and more"
+    },
+
+    {
+        id: "3",
+        icon: "",
+        title: "Progress Tracking",
+        description: "Keep track of your improvement over time with detailed analytics and performance insights"
+    },
+
+    {
+        id: "4",
+        icon: "",
+        title: "Accessible Anytime, Anywhere",
+        description: "Learnify is available on multiple platforms, allowing you to learn on the go, whenever and wherever you want"
+    },
+]
