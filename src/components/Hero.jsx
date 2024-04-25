86b12527107b1cb0650c9930e1e87d057f167925
Gihan Chamila <algihanchamila@gmail.com>
2024-04-25 19:01:41 +0530

    3 Star svgs added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import Section from './Section'
 import Button from './Button'
+import Stars from '../assets/Design/Stars'
 
 const Hero = () => {
 
@@ -8,12 +9,14 @@ const Hero = () => {
 
   return (
     <Section id={`hero`} className={`pt-[12rem] -mt-[5.25rem]`} customPadding={true} >
+      <Stars />
       <div className='container relative' ref={parallaxRef}>
         <div className=' justify-start relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]'>
           <h1 className="h1 mb-6">Ignite Your Intelligence with Learnify</h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-p-9 lg:mb-8">Our platform offers a dynamic range of courses and resources designed to fuel your curiosity and enhance your cognitive abilities. From mastering new skills to delving into fascinating subjects, Learnify provides the tools you need to expand your horizons and reach your full potential. With interactive lessons, expert guidance, and a supportive community, you'll embark on a transformative learning journey that empowers you to excel in every aspect of life. Join us today and unlock the power of your intellect</p>
           <Button primary={true} white={true}>Learn more</Button>
         </div>
+        
       </div>       
     </Section>
   )
