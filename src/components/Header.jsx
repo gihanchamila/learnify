e9b4ead92b9c44a922eae745339d1313c8a2f6a8
Gihan Chamila <algihanchamila@gmail.com>
2024-05-03 12:07:22 +0530

    Changed border weight

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
   }
 
   return (
-    <div className={`fixed w-full top-0 left-0 z-50 lg:border-b bg-white lg:border-p-10 lg:backdrop-blur-sm ${openNavigation ? "" :  "backdrop-blur-sm sm:bg-white"}`}>
+    <div className={`fixed w-full top-0 left-0 z-50 lg:border-b-2 bg-white lg:border-p-8 lg:backdrop-blur-sm ${openNavigation ? "" :  "backdrop-blur-sm sm:bg-white"}`}>
       <div className={`flex items-center justify-center w-full lg:px-7.5 xl:px-10 max-lg:py-4`}>
         <a href="/hero" className={`block xl:mr-[5rem] px-6 my-4 ${openNavigation ? "": "block"} `}>
           <img src={White} width={30} height={30} />
