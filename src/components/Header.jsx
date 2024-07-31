a2d4d7f1eacc62b2cb7e3cfe38a55d803852b857
Gihan Chamila <algihanchamila@gmail.com>
2024-07-31 10:42:28 +0530

    fixed errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,16 +29,15 @@ const Header = () => {
   }
 
   return (
-    <div className={`fixed w-full top-0 left-0 z-50 lg:border-b-2 bg-white lg:border-p-8 lg:backdrop-blur-sm ${openNavigation ? "" :  "backdrop-blur-sm sm:bg-white"}`}>
+    <div className={`fixed w-full top-0 left-0 z-50 border-1 lg:border-b-2 bg-white lg:border-p-8 lg:backdrop-blur-sm ${openNavigation ? "" :  "backdrop-blur-sm sm:bg-white"}`}>
       <div className={`flex items-center justify-center w-full lg:px-5.5 xl:px-10 max-lg:py-4`}>
         <a href="/hero" className={`block xl:mr-[5rem] px-6 my-4 ${openNavigation ? "": "block"} `}>
           <img src={White} width={30} height={30} />
         </a>
-        <nav className={`${openNavigation ? "flex bg-white " : "hidden"} fixed items-center justify-center top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
+        <nav className={`${openNavigation ? "flex bg-white " : "hidden"} fixed items-center justify-center top-[5rem] left-0 right-0 bottom-0 lg:static lg:m-auto lg:flex lg:mx-auto lg:bg-transparent leading-5`}>
           <div className='relative z-10 flex flex-col items-center justify-between m-auto lg:flex-row'>
-            
             {navigation.map((item) => (
-              <a key={item.id} href={item.url} onClick={handleClick}  className={`block relative font-code uppercase text-p-8 transition-colors hover:text-p-10 px-4 py-5 md:py-8 lg:-mr-0.25 lg:text-xs sm:text-xs lg:font-semibold  ${
+              <a key={item.id} href={item.url} onClick={handleClick}  className={`block relative font-code uppercase text-p-8 transition-colors hover:text-p-10 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs sm:text-xs lg:font-semibold  ${
                 item.onlyMobile ? "lg:hidden" : ""}  ${item.url === pathname.hash? "z-20 lg:text-p-10" : "lg:text-p-10/80"} lg:hover:text-p-10 xl:px-12`}>
                 {item.title}
               </a>
